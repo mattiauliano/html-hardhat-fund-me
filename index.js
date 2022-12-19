5368c7d42d0f40f1fe1c0e8c5e3bd42cb32eb090
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-19 05:53:41 +0100

    Connect wallet on click

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-// Check if exist a wallet connected to ethereum
-if (typeof window.ethereum !== "undefined") {
-  console.log("I see a metamask!");
-}
+// Select connect button
+const connectBtn = document.getElementById("connectBtn");
+
+const connect = async () => {
+  // Check if exist a wallet connected to ethereum
+  if (typeof window.ethereum !== "undefined") {
+    // Ask user to connect his wallet
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    connectBtn.innerText = "Connected";
+  } else {
+    connectBtn.innerText = "Wallet required";
+  }
+};
+
+// On click call connect function
+connectBtn.addEventListener("click", connect);
