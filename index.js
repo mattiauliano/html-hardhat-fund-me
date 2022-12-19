43f19dee7b264e01ed6a0a95c69c5741759e398f
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-19 06:14:50 +0100

    Import ethers library locally

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+// Import ethers (ONLY IN THIS LESSON)
+import { ethers } from "./ethers-5.1.esm.min.js";
+
 // Select connect button
 const connectBtn = document.getElementById("connectBtn");
 
 const connect = async () => {
-  // Check if exist a wallet connected to ethereum
-  if (typeof window.ethereum !== "undefined") {
-    // Ask user to connect his wallet
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-    connectBtn.innerText = "Connected";
-  } else {
-    connectBtn.innerText = "Wallet required";
-  }
+    // Check if exist a wallet connected to ethereum
+    if (typeof window.ethereum !== "undefined") {
+        // Ask user to connect his wallet
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        connectBtn.innerText = "Connected";
+    } else {
+        connectBtn.innerText = "Wallet required";
+    }
+};
+
+// Fund function
+const fund = async (ethAmount) => {
+    console.log(`Funding with ${ethAmount}`);
+    if (typeof window.ethereum !== "undefined") {
+    }
 };
 
 // On click call connect function
